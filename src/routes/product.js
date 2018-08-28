93ebe68df48a47da87e7efa124005ab48d195136
junchang.ma.ele_waimai <junchang.ma@ele.me>
2018-08-28 20:33:56 +0800

    首次提交

---
+++ src/routes/product.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {connect} from 'dva'
+import ProductList from '../components/ProductList'
+
+const Products = ({dispatch, products}) => {
+  function handleDelete (id) {
+    dispatch({
+      type: 'products/delete',
+      payload: id
+    })
+  }
+
+  return (
+    <div>
+      <h2>List of Products</h2>
+      <ProductList onDelete={handleDelete} products={products} />
+    </div>
+
+  )
+}
+
+export default connect(({products}) => {
+  return {
+    products
+  }
+})(Products)
